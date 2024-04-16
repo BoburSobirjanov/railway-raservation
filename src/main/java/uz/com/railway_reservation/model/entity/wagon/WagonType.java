932734cd7319c828234bb9d490a4ef393eba8c4c
Bobur Sobirjanov <sobirjanovbobur768@gmail.com>
2024-04-16 19:04:49 +0500

    last changes for price

--- src/main/java/uz/com/railway_reservation/model/entity/wagon/WagonType.java
+++ src/main/java/uz/com/railway_reservation/model/entity/wagon/WagonType.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum WagonType {
 
-    BOXCAR(700.0),
-    FLATCAR(310.0),
-    GONDOLACAR(420.0),
-    HOPPERCAR(550.0),
-    REEFER(680.0),
-    AUTORACK(800.0);
+    BOXCAR(7.0),
+    FLATCAR(3.0),
+    GONDOLACAR(4.0),
+    HOPPERCAR(5.0),
+    REEFER(6.0),
+    AUTORACK(8.0);
 
     private final double amount;
 }
