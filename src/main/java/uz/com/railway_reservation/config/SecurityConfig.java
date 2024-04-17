46ce0bf21ee2861ce7138383577d22252d9a9876
Bobur Sobirjanov <sobirjanovbobur768@gmail.com>
2024-04-17 14:56:43 +0500

    cors added

--- src/main/java/uz/com/railway_reservation/config/SecurityConfig.java
+++ src/main/java/uz/com/railway_reservation/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package uz.com.railway_reservation.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((requestsConfigurer) ->
                         requestsConfigurer
                                 .requestMatchers(permitAll).permitAll()
